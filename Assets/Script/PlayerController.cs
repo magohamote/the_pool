9e7915cc21803bd6975591b314748f469a915345
Joachim Hugonot <joachim.hugonot@epfl.ch>
2014-05-08 15:04:46 +0200

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+	
+	
+	public float speed;
+	
+	void FixedUpdate ()
+	{
+		
+		float turn = Input.GetAxis("Horizontal");
+
+   
+		transform.Rotate(0, turn * 90 * Time.deltaTime, 0);
+		
+		
+		Vector3 moveDirection = transform.forward * Input.GetAxis("Vertical") * -1 * speed;
+        
+    
+		if (Input.GetKey ("space")) {
+            moveDirection.y = 0;
+        }
+		// Move the controller
+		rigidbody.AddForce(moveDirection * Time.deltaTime);
+		
+		
+		
+		
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+		}
+	}
+}
+
+
---
+++ Assets/Script/Rotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(10,20,30)*Time.deltaTime);
+	}
+}
