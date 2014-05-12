c655303b2437dbdea77caf04ac47ab0f4e9d7c6f
Christopher Carballo <carballo.christopher@gmail.com>
2014-05-12 17:55:17 +0200

    Fixed camera

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -6,13 +6,16 @@ public class CameraController : MonoBehaviour {
 	
 	public GameObject player;
 	private Vector3 offset;
+	private Quaternion offsetRot;
 	
 	void Start () {
 		offset =  new Vector3 (0f,2.42f,0f);
+		offsetRot = player.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
 		transform.position = player.transform.position + offset;
+		transform.rotation = player.transform.rotation * offsetRot;
 	}
 }
